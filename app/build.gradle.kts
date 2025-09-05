818206b45ff77164eb8bdfd9f9e5829362bc6063
fraigneau <fraigneaulucas@proton.me>
2025-09-05 10:48:59 +0200

    feat(deps): add room for SQL base

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    id("org.jetbrains.kotlin.kapt")
 }
 
 android {
@@ -54,6 +55,10 @@ dependencies {
 
     implementation("androidx.datastore:datastore-preferences:1.1.7")
     implementation(libs.androidx.appcompat)
+
+    implementation("androidx.room:room-runtime:2.6.1")
+    kapt("androidx.room:room-compiler:2.6.1")
+    implementation("androidx.room:room-ktx:2.6.1")
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
